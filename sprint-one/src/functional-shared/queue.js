6d2473ec3323b406edded54ea9dfec2555236f77
eugenia ong <eugenia.osy@gmail.com>
2019-04-10 12:28:22 -0700

    Complete functional-shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function () {
+  var someInstance = {};
+  _.extend(someInstance, queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (value) {
+    var last = Object.keys(this.storage).length;
+    this.storage[last] = value;
+  },
+
+  dequeue: function () {
+    var result = this.storage[0];
+    for (var key in this.storage) {
+      newIndex = Number(key) + 1;
+      this.storage[key] = this.storage[newIndex];
+    }
+    var last = Object.keys(this.storage).length;
+    delete this.storage[last - 1];
+    return result;
+  },
+
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+
+  var someInstance = {};
+  _.extend(someInstance, stackMethods);
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (value) {
+    for (var key in this.storage) {
+      var nextIndex = Number(key) + 1;
+      this.storage[nextIndex] = this.storage[key];
+    }
+    this.storage[0] = value;
+  },
+
+  pop: function () {
+    var result = this.storage[0];
+    for (var key in this.storage) {
+      if (key === '0') {
+        continue;
+      } else {
+        var prevIndex = Number(key) - 1;
+        this.storage[prevIndex] = this.storage[key];
+      }
+    }
+    var last = Object.keys(this.storage).length;
+    delete this.storage[last - 1];
+    return result;
+  },
+
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+
+
+};
 
 
