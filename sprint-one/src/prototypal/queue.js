cb10a7c68c88bd9360672cc12434f73cc5cb13e0
eugenia ong <eugenia.osy@gmail.com>
2019-04-10 12:28:51 -0700

    Complete prototypal queue and stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function () {
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  var last = Object.keys(this.storage).length;
+  this.storage[last] = value;
+};
+
+queueMethods.dequeue = function () {
+  var result = this.storage[0];
+  for (var key in this.storage) {
+    newIndex = Number(key) + 1;
+    this.storage[key] = this.storage[newIndex];
+  }
+  var last = Object.keys(this.storage).length;
+  delete this.storage[last - 1];
+  return result;
+};
+
+queueMethods.size = function () {
+  return Object.keys(this.storage).length;
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,37 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function (value) {
+  for (var key in this.storage) {
+    var nextIndex = Number(key) + 1;
+    this.storage[nextIndex] = this.storage[key];
+  }
+  this.storage[0] = value;
+};
+
+stackMethods.pop = function () {
+  var result = this.storage[0];
+  for (var key in this.storage) {
+    if (key === '0') {
+      continue;
+    } else {
+      var prevIndex = Number(key) - 1;
+      this.storage[prevIndex] = this.storage[key];
+    }
+  }
+  var last = Object.keys(this.storage).length;
+  delete this.storage[last - 1];
+  return result;
+};
+
+stackMethods.size = function () {
+  return Object.keys(this.storage).length;
+};
+
 
 
