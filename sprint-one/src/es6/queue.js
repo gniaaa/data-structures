0468c6bcee6c981593380852bf0efbbaae1074ff
eugenia ong <eugenia.osy@gmail.com>
2019-04-10 12:29:49 -0700

    Complete es6 queue and stack

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  enqueue(value) {
+    var last = Object.keys(this.storage).length;
+    this.storage[last] = value;
+  }
+
+  dequeue() {
+    var result = this.storage[0];
+    for (var key in this.storage) {
+      var newIndex = Number(key) + 1;
+      this.storage[key] = this.storage[newIndex];
+    }
+    var last = Object.keys(this.storage).length;
+    delete this.storage[last - 1];
+    return result;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,33 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    for (var key in this.storage) {
+      var nextIndex = Number(key) + 1;
+      this.storage[nextIndex] = this.storage[key];
+    }
+    this.storage[0] = value;
+  }
+
+  pop() {
+    var result = this.storage[0];
+    for (var key in this.storage) {
+      if (key === '0') {
+        continue;
+      } else {
+        var prevIndex = Number(key) - 1;
+        this.storage[prevIndex] = this.storage[key];
+      }
+    }
+    var last = Object.keys(this.storage).length;
+    delete this.storage[last - 1];
+    return result;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
