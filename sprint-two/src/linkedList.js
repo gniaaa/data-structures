86812241e5eada5418f1873900caa6aee8235987
eugenia ong <eugenia.osy@gmail.com>
2019-04-10 15:47:08 -0700

    Complete linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,21 +1,47 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  list.addToTail = function (value) {
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    if (list.head === null) {
+      return -1;
+    }
+
+    var currentHead = list.head;
+    list.head = currentHead.next;
+    currentHead.next = null;
+    return currentHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function (target) {
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return false;
+
   };
 
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
 
   node.value = value;
