c6f89ac58034b26177e24d6b1490ef8fcc321de4
eugenia ong <eugenia.osy@gmail.com>
2019-04-10 12:29:29 -0700

    Complete pseudoclassical queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,23 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function () {
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function (value) {
+  var last = Object.keys(this.storage).length;
+  this.storage[last] = value;
+};
 
+Queue.prototype.dequeue = function () {
+  var result = this.storage[0];
+  for (var key in this.storage) {
+    newIndex = Number(key) + 1;
+    this.storage[key] = this.storage[newIndex];
+  }
+  var last = Object.keys(this.storage).length;
+  delete this.storage[last - 1];
+  return result;
+};
+
+Queue.prototype.size = function () {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,31 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  this.storage = {};
 };
 
+Stack.prototype.push = function (value) {
+  for (var key in this.storage) {
+    var nextIndex = Number(key) + 1;
+    this.storage[nextIndex] = this.storage[key];
+  }
+  this.storage[0] = value;
+};
+
+Stack.prototype.pop = function () {
+  var result = this.storage[0];
+  for (var key in this.storage) {
+    if (key === '0') {
+      continue;
+    } else {
+      var prevIndex = Number(key) - 1;
+      this.storage[prevIndex] = this.storage[key];
+    }
+  }
+  var last = Object.keys(this.storage).length;
+  delete this.storage[last - 1];
+  return result;
+};
+
+Stack.prototype.size = function () {
+  return Object.keys(this.storage).length;
+};
 
